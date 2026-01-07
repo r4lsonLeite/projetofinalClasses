e4f0c5172919b6c126435a973a177dd75a50ee7e
unknown <marcosfe54321@gmai.comgit config --global user.name  Marcos Felipe Ferreira Duartegit commit -m class usuario>
2026-01-07 19:41:32 -0300

    class usuario

---
+++ usuario.py
@@ -0,0 +1,23 @@
+class Usuario:
+    def __init__(self, nome, idade, moradia, area_util, experiencia, criancas, outros_animais):
+        self.nome = nome
+        self.idade = idade
+        self.moradia = moradia 
+        self.area_util = area_util
+        self.experiencia = experiencia
+        self.criancas = criancas  
+        self.outros_animais = outros_animais
+    def cadastrar(self):
+        print(f" Usuário {self.nome} cadastrado com sucesso!")
+    def editar(self, novo_nome, nova_idade, nova_moradia, nova_area, nova_experiencia, tem_criancas, novos_outros_animais):
+        self.nome = novo_nome
+        self.idade = nova_idade
+        self.moradia = nova_moradia
+        self.area_util = nova_area
+        self.experiencia = nova_experiencia
+        self.criancas = tem_criancas
+        self.outros_animais = novos_outros_animais
+        print(f" Dados de {self.nome} atualizados!")
+
+
+    
