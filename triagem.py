b053ef4726931fcc56c3698e5d22c2d7163ce6c0
r4ilsonLeite <railsonleite@gmail.com>
2026-01-07 21:19:57 -0300

    classe triagem

--- triagem.py
+++ triagem.py
@@ -1,4 +1,10 @@
+from usuario import usuario
+from animais import animais
+
 
 class triagem:
     def __init__(self, pontuacao:str, elegivel:bool, observacoes:str):
+        self.pontuacao = pontuacao
+        self.elegivel = elegivel
+        self.observacoes = observacoes
         
